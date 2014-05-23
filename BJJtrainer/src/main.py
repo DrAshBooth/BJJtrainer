4bb91eee2f5c1d97fb71a5fd94d0dc8758384995
ab24v07 <ash.booth@soton.ac.uk>
2014-05-23 19:16:29 +0100

    added links to main

--- BJJtrainer/src/main.py
+++ BJJtrainer/src/main.py
@@ -2,6 +2,9 @@
 Created on 21 May 2014
 
 @author: Ash
+
+http://www.ibm.com/developerworks/linux/library/l-python-state/index.html
+http://python-3-patterns-idioms-test.readthedocs.org/en/latest/StateMachine.html
 '''
 
 from trainer import Game
